1ae9a653c872cd945b9ab742ebbfc0e9a98a91b7
saied-nasiripour <saied.nasiripour@gmail.com>
2024-04-28 15:07:30 +0330

    add winning combinations

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log.jsx";
+import {WINNING_COMBINATIONS} from "./winning-combinations.js";
 
 function deriveActivePlayer(gameTurns) {
     /*console.log("prevTurns - gameTurns: ", gameTurns);
